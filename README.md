5235cad6a897e310fd4f9f06f97a0628df906733
yousef-ghanim <yousefghanim85@gmail.com>
2021-04-12 12:54:24 +0000

    intial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A large-scale JavaScript project, as a final project of JS Udemy course.
---
+++ src/js/models/Recipe.js
@@ -0,0 +1,111 @@
+import axios from "axios";
+
+export default class Recipe {
+  constructor(id) {
+    this.id = id;
+  }
+  async getRecipe() {
+    try {
+      const res = await axios(
+        `https://forkify-api.herokuapp.com/api/get?rId=${this.id}`
+      );
+      this.title = res.data.recipe.title;
+      this.author = res.data.recipe.publisher;
+      this.img = res.data.recipe.image_url;
+      this.url = res.data.recipe.source_url;
+      this.ingredients = res.data.recipe.ingredients;
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+      alert("something went wrong");
+    }
+  }
+  calcTime() {
+    const numImg = this.ingredients.length;
+    const periods = Math.ceil(numImg / 3);
+    this.time = periods * 15;
+  }
+  calcServings() {
+    this.servings = 4;
+  }
+  parseIngredients() {
+    const unitsLong = [
+      "tablespoons",
+      "tablespoon",
+      "ounces",
+      "ounce",
+      "teaspoons",
+      "teaspoon",
+      "cups",
+      "pounds",
+    ];
+    const unitsShort = [
+      "tbsp",
+      "tbsp",
+      "oz",
+      "oz",
+      "tsp",
+      "tsp",
+      "cup",
+      "pound",
+    ];
+    const units = [...unitsShort, "kg", "g"];
+    const newIngredients = this.ingredients.map((el) => {
+      // Uniform units
+      let ingredient = el.toLowerCase();
+      unitsLong.forEach((unit, i) => {
+        ingredient = ingredient.replace(unit, unitsShort[i]);
+      });
+      // Remove parentheses
+      ingredient = ingredient.replace(/ *\([^)]*\) */g, " ");
+      // Parse ingredients into count, unit and ingredient
+      const arrIng = ingredient.split(" ");
+      const unitIndex = arrIng.findIndex((e12) => units.includes(e12));
+      let objIng;
+      if (unitIndex > -1) {
+        // There is a unit
+        const arrCount = arrIng.slice(unitIndex);
+
+        let count;
+        if (arrCount.length === 1) {
+          count = eval(arrIng[0].replace("-", "+"));
+        } else {
+          count = eval(arrIng.slice(0, unitIndex).join("+"));
+        }
+
+        objIng = {
+          count,
+          unit: arrIng[unitIndex],
+          ingredient: arrIng.slice(unitIndex + 1).join(" "),
+        };
+      } else if (parseInt(arrIng[0], 10)) {
+        // there is NO unit, but 1st element is number
+        objIng = {
+          count: parseInt(arrIng[0], 10),
+          unit: "",
+          ingredient: arrIng.slice(1).join(" "),
+        };
+      } else if (unitIndex === -1) {
+        // There is NO unit and NO number in 1st position
+        objIng = {
+          count: 1,
+          unit: "",
+          ingredient,
+        };
+      }
+
+      return objIng;
+    });
+    this.ingredients = newIngredients;
+  }
+
+  updateServings(type) {
+    // Servings
+    const newServings = type === "dec" ? this.servings - 1 : this.servings + 1;
+    // Ingredients
+    this.ingredients.forEach((ing) => {
+      ing.count *= newServings / this.servings;
+    });
+    this.servings = newServings;
+  }
+}
